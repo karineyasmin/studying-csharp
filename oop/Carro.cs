4d54a40a293ad0a7d30a981402a86835019ae9df
Karine Yasmin <130607138+karineyasmin@users.noreply.github.com>
2025-01-10 18:55:37 +0000

    add

--- oop/Carro.cs
+++ oop/Carro.cs
@@ -6,17 +6,48 @@ namespace oop
         // Atributo de instância
         public string cor;
         public string modelo;
-        public string velocidade;
+        public int velocidade;
 
 
-        // Atributo estático - atributo pertencente a classe
-        public int numeroDeInstancias;
 
         // modificadores de acesso
         // private - o acesso é feito apenas de dentro da própria classe
         // public - o acesso é permitido de qualquer parte do código
         // internal - o acesso é permitido apenas de dentro do assembly
         // protected - o acesso é permitido apenas a classe e as classes derivadas
+
+        public void Acelerar()
+        {
+            Console.WriteLine("Carro acelerando...");
+            velocidade += 1;
+        }
+
+        public void Frear()
+        {
+            Console.WriteLine("Carro freando...");
+            velocidade -= 1;
+        }
+
+        public int ConsultarVelocidade()
+        {
+            return velocidade;
+        }
+
+        public void Virar(string direcao)
+        {
+            Console.WriteLine($"Carro virando para a {direcao}");
+        }
+
+        public void Parar()
+        {
+            while (velocidade > 0)
+            {
+                velocidade -= 1;
+            }
+
+            Console.WriteLine("Carro parado")
+        }
+
 
     }
 }
--- oop/Program.cs
+++ oop/Program.cs
@@ -4,18 +4,26 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        int valor = 1;
+        Carro fusca = new();
 
-        Carro carro1 = new Carro();
-        Carro carro2 = new();
-        var carro3 = new Carro();
 
-        carro1.cor = "Verde";
-        carro2.cor = "Amarela";
-        carro3.cor = "Vermelho";
+        fusca.modelos = "1300";
+        fusca.cor = "Preto";
 
-        Carro.numeroDeInstancias = 2;
+        fusca.Acelerar();
+        fusca.Acelerar();
 
+        var velocidade = fusca.ConsultarVelocidade();
+        Console.WriteLine($"A velocidade atual é de {velocidade} km/h");
+        fusca.Frear();
+        fusca.Virar("direita");
+        fusca.Acelerar();
+        fusca.Frear();
+        fusca.Virar("esquerda")
+        fusca.Parar();
+
+
+        Console.ReadKey();
 
     }
 }
